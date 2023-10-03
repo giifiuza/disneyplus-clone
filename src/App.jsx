4e33953bbc6c45bf48dd52568afe8c83f514da75
Giovanna Fiuza <94703688+giifiuza@users.noreply.github.com>
2023-10-02 22:41:08 -0300

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/Header'
+
+function App() {
+  
+
+  return (
+    <div className="">
+      <Header />
+    </div> 
+    
+  )
+}
+
+export default App
